cc311cc53d135a73cbd9ac505407af536089738b
urprakashgupta <urprakashgupta@gmail.com>
2025-08-12 13:56:16 +0530

    basic setup with database initilization

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Button> Hii there </Button>
+    </div>
+  );
+}
